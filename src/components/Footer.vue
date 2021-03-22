a696b116010496e459318ee4adbedbaea6c028c0
michaelmurwayi <mikemurwayi7@gmail.com>
2021-03-22 21:17:11 +0300

    adding link place holders in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,66 @@
 <template>
   <v-app>
     <v-footer dark padless style="width:100vw position:absolute; top:95vh;">
-    <div class="col-md-12" style="background-color:#0074a5;">
+    <div class="col-md-12 row" style="background-color:#0074a5;">
+      <div class="col-md-3">
+        <v-app-bar-title class="ml-3" >Contact Us </v-app-bar-title>
+        <div class="col-md-8">
+        <p>Odex Chemicals Limited,
+          Kyang’ombe Road, Off Mombasa Road,
+          Behind Ramtons, Opposite Clique,
+          P.O Box 72390 – 00200,
+          Nairobi, Kenya.
+          Tel: 020-2347131/32
+          Mobile: 0713064662
+          Email: info@odexchem.co.ke
+      </p>
+      </div>
+      </div>
+
+      <div class="col-md-3">
+        <v-app-bar-title class="ml-3" >Product Range </v-app-bar-title>
+        <div class="col-md-8">
+        <p>Odex Chemicals Limited,
+          Kyang’ombe Road, Off Mombasa Road,
+          Behind Ramtons, Opposite Clique,
+          P.O Box 72390 – 00200,
+          Nairobi, Kenya.
+          Tel: 020-2347131/32
+          Mobile: 0713064662
+          Email: info@odexchem.co.ke
+      </p>
+      </div>
+      </div>
+
+      <div class="col-md-3">
+        <v-app-bar-title class="ml-3" >OTHER LINKS</v-app-bar-title>
+        <div class="col-md-8">
+        <p>Odex Chemicals Limited,
+          Kyang’ombe Road, Off Mombasa Road,
+          Behind Ramtons, Opposite Clique,
+          P.O Box 72390 – 00200,
+          Nairobi, Kenya.
+          Tel: 020-2347131/32
+          Mobile: 0713064662
+          Email: info@odexchem.co.ke
+      </p>
+      </div>
+      </div>
+
+      <div class="col-md-3">
+        <v-app-bar-title class="ml-3" >SUBSCRIBE TO NEWSLETTER </v-app-bar-title>
+        <div class="col-md-8">
+        <p>Odex Chemicals Limited,
+          Kyang’ombe Road, Off Mombasa Road,
+          Behind Ramtons, Opposite Clique,
+          P.O Box 72390 – 00200,
+          Nairobi, Kenya.
+          Tel: 020-2347131/32
+          Mobile: 0713064662
+          Email: info@odexchem.co.ke
+      </p>
+      </div>
+      </div>
 
     </div>
     <div class="col-md-12 text-center" style="background-color: #28b6e7">
