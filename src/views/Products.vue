21db40203ae28558e007d1dc81eda870d3de3ffc
michaelmurwayi <mikemurwayi7@gmail.com>
2021-03-22 21:30:15 +0300

    products page initial template

---
+++ src/views/Products.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Navbar/>
+    <Footer/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Navbar from '@/components/Navbar.vue'; // @ is an alias to /src
+import Footer from '@/components/Footer.vue'
+
+@Component({
+  components: {
+    Navbar,
+    Footer
+  },
+})
+export default class Home extends Vue {}
+</script>
