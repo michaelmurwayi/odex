29932a1b8e4c88cb3dc0b2222d18f3e65761d1c7
michaelmurwayi <mikemurwayi7@gmail.com>
2021-03-22 21:35:18 +0300

    initial service page template

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Services from '../views/Services.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,14 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Services
   }
 ]
 
