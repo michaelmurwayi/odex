5f103dd9e8e4ebcbb080b0330792c40211e6712c
michaelmurwayi <mikemurwayi7@gmail.com>
2021-03-22 15:59:01 +0300

    first  footer component design

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-footer dark padless style="width:100vw position:absolute; top:95vh;">
+    <div class="col-md-12" style="background-color:#0074a5;">
+
+    </div>
+    <div class="col-md-12 text-center" style="background-color: #28b6e7">
+    <p> Copyright © 2020. Odex Chemicals Limited. All Rights Reserved.</p>
+        
+    </div>
+    </v-footer>
+    
+    
+  </v-app>
+</template>
+<style scoped>
+
+</style>
+<script lang="ts">
+export default {
+  name: 'Footer',
+    data(){
+      return{
+        socials: ['SignUp','LogIn','Messenger', 'FacebookLite','Watch','People','Pages','Page   categories','Places','Games','Locations','Marketplace','FacebookPay','Groups','Jobs','Oculus','Portal','Instagram','Local','Fundraisers','Services','Voting Information Centre','About','Create ad' ,'Create Page','Developers','Careers','Privacy','Cookies','AdChoices','Terms','Help'],
+      };
+    }
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Navbar/>
+    <Footer/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Navbar from '@/components/Navbar.vue'; // @ is an alias to /src
+import Footer from '@/components/Footer.vue'
 
 @Component({
   components: {
     Navbar,
+    Footer
   },
 })
 export default class Home extends Vue {}
