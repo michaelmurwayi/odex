e1909d6afb314fc4faf94e79d98dee10dc63021e
michaelmurwayi <mikemurwayi7@gmail.com>
2021-03-25 21:28:16 +0300

    adding service page

---
+++ src/components/servicesMain.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-app  style="position:relative; top:-650px; bottom:20vh; width:100%;">
+    <div class="Main">
+    <v-carousel hide-delimiters :controls="false" :indicators="false" :interval="3000" direction="up" style="height:400px"
+    >
+    <v-carousel-item
+      v-for="(item,i) in slides"
+      :key="i"
+      :src= "require('../assets/' + item)"
+    >
+    
+    <v-card class="" style=" postion:absolute; top:150px; left:650px; height:15vh; width: fit-content; background-color:#28b6e7; opacity:0.8;">
+        <v-card-title class="" style="position:relative; left:0px; top:20px; font-weight:bold; font-size: 8vh;">Services</v-card-title>
+    </v-card>
+    
+    </v-carousel-item>
+    </v-carousel>
+    </div>
+    <div class="services col-md-12  row container-fluid"  style="position:relative; top:10vh; right:0px; left:50px;" v-for="service in services" :key="service">
+        <ul style="line-height: 2.0em;">
+            <li>
+                {{ service  }}
+            </li>
+        </ul>
+    </div>
+    <div style="position:relative; top:15vh; right:0px; left:50px;">
+
+        <p> We are committed to doing this by:</p>
+    </div>
+    <div v-for="commit in commits" :key="commit" style="position:relative; top:15vh; right:0px; left:50px;">
+        <ul style="line-height: 2.0em;">
+            <li>
+                {{ commit  }}
+            </li>
+        </ul>
+    </div>
+    </v-app>
+</template>
+<script>
+export default {
+     data () {
+      return {
+        slides: [
+            'servicesmain.jpg',
+            
+        ],
+        services:[
+            'Supply of quality food grade detergents to cater for all cleaning & hygiene needs.',
+            'Preparation of a detailed cleaning manual for the factories to ensure a harmonized cleaning and hygiene system.',
+            'Technical support to ensure good and consistent cleaning and hygiene results and to resolve quality challenges encountered from time to time.',
+            'Analysis of utilities water( boilers, cooling towers, pasteurizers, ETP and process water treatment to confirm that the treatment regime are delivering the desired results.',
+            'Quarterly trainings to ensure continuous improvement in the factories.',
+            'Supply of high quality dosing and control equipment to ensure accurate and consistent dosing of the chemicals.',
+            'Regular visit to the factory to monitor the cleaning and hygiene standards, and to identify any areas requiring urgent attention.',
+            'Reports to the management indicating the levels of hygiene, cost trends against the budget, projects in progress and recommendations for improvements.',
+            'Periodic audits for the entire factory to identify major improvement projects.'
+            ],
+        commits: [
+            'Investing the time to listen, understand and respond to customers’ unique cleaning and hygiene needs.',
+            'Taking a personal interest to ensure the facilities they care for are consistently clean, safe and attractive.',
+            'Pursuing innovation in every form likely to make the lives of our customers simpler and more profitable.',
+            'Customer collaborating to exceed their expectations, every day, everywhere.'
+            ],
+
+        products_lable: [
+            'KITCHEN HYGIENE',
+            'HOUSE KEEPING',
+            'FOOD & BEVERAGE  ',
+            'CONVEYER SYSTEM',
+            'PERSONAL CARE',
+            'WORKSHOP',
+            'LAUNDRY',
+            'WATER TREATMENT',
+            'SPECIALTIES',
+        ],
+      
+}
+     }
+     }
+</script>
+<style scoped>
+.products_card{
+    margin-left:25px;
+    margin-top: 10px;
+    width:15vw;
+    height:250px;
+    background-color: #CCCCFF;
+    display:flex;
+}
+.container{
+  height:400px;
+  display: "flex"
+}
+</style>
